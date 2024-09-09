ed68596c222e5021d5981bce10152e72759c27f6
Tanmay Borse <tanmayborse28@gmail.com>
2024-09-09 12:27:57 +0530

    Project Setup

---
+++ app/layout.tsx
@@ -0,0 +1,40 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import {IBM_Plex_Sans} from "next/font/google";
+import "./globals.css";
+import { cn } from "@/lib/utils";
+
+
+const IBMPlex = IBM_Plex_Sans({
+  subsets: ["latin"],
+  weight: ['400','500','600','700'],
+  variable: '--font-ibm-plex'
+});
+
+// const geistSans = localFont({
+//   src: "./fonts/GeistVF.woff",
+//   variable: "--font-geist-sans",
+//   weight: "100 900",
+// });
+// const geistMono = localFont({
+//   src: "./fonts/GeistMonoVF.woff",
+//   variable: "--font-geist-mono",
+//   weight: "100 900",
+// });
+
+export const metadata: Metadata = {
+  title: "Imaginify",
+  description: "AI powered Saas Platform",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={cn("font-IBMPlex antialiased", IBMPlex.variable)}> {children} </body>
+    </html>
+  );
+}
